f68c7df0a0a4a6c5fb111f42f1ee8705414e1512
Joseph Angeletti <jangelet@gmail.com>
2017-10-30 21:01:37 -0400

    1.1 and 1.2

---
+++ src/PermCheck.java
@@ -0,0 +1,40 @@
+/**
+ * Created by JANG3LET on 10/30/17.
+ */
+
+import java.util.*;
+
+public class PermCheck {
+
+    private static boolean stringLength(String s1, String s2){
+        if(s1.length() == s2. length()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean isPermutation(String s1, String s2){
+        if(stringLength(s1, s2)){
+            char [] charArr1 = s1.toCharArray();
+            char [] charArr2 = s2.toCharArray();
+            Arrays.sort(charArr1);
+            Arrays.sort(charArr2);
+
+            if(Arrays.equals(charArr1, charArr2)){
+               return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        System.out.println(isPermutation(s1, s2));
+    }
+}
---
+++ src/UniqueStringCheck.java
@@ -0,0 +1,41 @@
+/**
+ * Created by JANG3LET on 10/29/17.
+ */
+
+import java.util.*;
+
+public class UniqueStringCheck {
+
+    public static boolean checksString(String s1){
+        HashMap<Character, Integer> charCounter =
+                new HashMap<Character, Integer>();
+        boolean unique = false;
+
+        for (char ch : s1.toCharArray())
+            charCounter.put(ch, charCounter.containsKey(ch) ?
+                    (charCounter.get(ch) + 1) : 1);
+
+        for(int val : charCounter.values()) {
+            if(val > 1){
+                unique = false;
+                break;
+            }else{
+                unique = true;
+            }
+        }
+
+
+        System.out.println(charCounter.entrySet());
+        return unique;
+    }
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        String s1 = s.nextLine();
+
+        System.out.println(checksString(s1));
+    }
+
+
+}
